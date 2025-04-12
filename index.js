6af768ae9af3ac7a7fb9c1c0e7e6c8f47b717234
YspaceBR <andredrico@gmail.com>
2025-04-12 12:11:53 -0300

    db

--- index.js
+++ index.js
@@ -31,10 +31,10 @@ app.set("views", path.join(__dirname, "views"));
 app.use(express.static(path.join(__dirname, "assets")));
 
 const conn = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "nodemysql2",
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
 });
 
 conn.connect((err) => {
