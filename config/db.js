e81f4e4052984a10a54932e8a073d716a954a907
Andriel <andredrico@gmail.com>
2025-05-30 17:57:34 -0300

    db atualizado

--- config/db.js
+++ config/db.js
@@ -1,26 +1,23 @@
+const mysql = require("mysql2");
 require("dotenv").config();
-const mysql = require('mysql2');
 
-const pool = mysql.createPool({
+// Conexão com o MySQL
+const conn = mysql.createConnection({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
   port: process.env.DB_PORT,
-  timezone: '-03:00',
-  waitForConnections: true,
-  connectionLimit: 10,
-  queueLimit: 0
+  timezone: '-03:00'
 });
 
-// Testar conexão
-pool.getConnection((err, connection) => {
+// Conectar ao banco de dados MySQL
+conn.connect((err) => {
   if (err) {
-    console.error("Erro ao conectar ao MySQL:", err);
-  } else {
-    console.log("Conectado ao MySQL!");
-    connection.release(); 
+    console.log("Erro ao conectar ao MySQL:", err);
+    return;
   }
+  console.log("Conectado ao MySQL!");
 });
 
-module.exports = pool;
+module.exports = conn;
