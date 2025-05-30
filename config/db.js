e11aa5c824f2d9cacd6473679b5643bcc48f0de6
Andriel <andredrico@gmail.com>
2025-05-30 19:12:04 -0300

    db atualizado

--- config/db.js
+++ config/db.js
@@ -6,8 +6,10 @@ const pool = mysql.createPool({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
+  port: process.env.DB_PORT,   // <-- faltando aqui
   timezone: '-03:00'
 });
+
 
 pool.getConnection((err, conn) => {
   if (err) {
