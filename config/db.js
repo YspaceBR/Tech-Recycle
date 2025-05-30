552fd885f52849fe1ce56175c9825a77fe2df92a
Andriel <andredrico@gmail.com>
2025-05-30 18:12:48 -0300

    db atualizado

--- config/db.js
+++ config/db.js
@@ -1,23 +1,20 @@
-require("dotenv").config(); // isso precisa ser a primeira linha
-const mysql = require("mysql2");
+const mysql = require('mysql2');
+require('dotenv').config();
 
 const pool = mysql.createPool({
-  host: process.env.DB_HOST,       // mysql.railway.internal
-  user: process.env.DB_USER,       // root
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,   // railway
-  port: process.env.DB_PORT,       // 3306
-  timezone: "-03:00",
-  waitForConnections: true,
-  connectionLimit: 10,
-  queueLimit: 0,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
+  timezone: '-03:00'
 });
 
 pool.getConnection((err, conn) => {
   if (err) {
-    console.error("Erro ao conectar ao MySQL:", err);
+    console.log('Erro ao conectar ao MySQL:', err);
   } else {
-    console.log("Conectado ao MySQL!");
+    console.log('Conectado ao MySQL!');
     conn.release();
   }
 });
