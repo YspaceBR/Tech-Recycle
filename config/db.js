c6ce94cee724399e9ee70febd92414b5a710b47d
Andriel <andredrico@gmail.com>
2025-05-30 18:46:32 -0300

    db atualizado

--- config/db.js
+++ config/db.js
@@ -2,19 +2,18 @@ const mysql = require('mysql2');
 require('dotenv').config();
 
 const pool = mysql.createPool({
-  host: process.env.DB_HOST,       // deve ser shinkansen.proxy.rlwy.net
-  user: process.env.DB_USER,       // root
-  password: process.env.DB_PASSWORD, 
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  port: Number(process.env.DB_PORT),  // 48964 — cuidado com o tipo (string -> number)
   timezone: '-03:00'
 });
 
 pool.getConnection((err, conn) => {
   if (err) {
-    console.log('Erro ao conectar ao MySQL:', err);
+    console.error('Erro ao conectar ao MySQL:', err);
   } else {
-    console.log('Conectado ao MySQL!');
+    console.log('Conectado ao MySQL !');
     conn.release();
   }
 });
